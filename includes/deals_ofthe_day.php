62e7d7b023be49474288684b6b11155d749911b6
Krushna Kumavat <Krushnakumavat93@gmail.com>
2024-03-30 16:07:54 +0530

    First Commit

---
+++ includes/deals_ofthe_day.php
@@ -0,0 +1,94 @@
+<div class="advance-product-box">
+                                <div class="biolife-title-box bold-style biolife-title-box__bold-style">
+                                    <h3 class="title">Deals of the day</h3>
+                                </div>
+                                <ul class="products biolife-carousel nav-top-right nav-none-on-mobile" data-slick='{"arrows":true, "dots":false, "infinite":false, "speed":400, "slidesMargin":30, "slidesToShow":1}'>
+                                    <li class="product-item">
+                                        <div class="contain-product deal-layout contain-product__deal-layout">
+                                            <div class="product-thumb">
+                                                <a href="#" class="link-to-product">
+                                                    <img src="assets/images/home-03/img2.jpg" alt="dd" width="330" height="330" class="product-thumnail">
+                                                </a>
+                                                <div class="labels">
+                                                    <span class="sale-label">-50%</span>
+                                                </div>
+                                            </div>
+                                            <div class="info">
+                                                <div class="biolife-countdown" data-datetime="2024-05-18 00:00 +00:00"></div>
+                                                <b class="categories">Fresh Tshirt</b>
+                                                <h4 class="product-title"><a href="#" class="pr-name">Black T shirt</a></h4>
+                                                <div class="price ">
+                                                    <ins><span class="price-amount"><span class="currencySymbol">Rs</span>500.00</span></ins>
+                                                    <del><span class="price-amount"><span class="currencySymbol">Rs</span>700.00</span></del>
+                                                </div>
+                                                <div class="slide-down-box">
+                                                    <p class="message">All products are carefully selected to ensure safety.</p>
+                                                    <div class="buttons">
+                                                        <a href="#" class="btn wishlist-btn"><i class="fa fa-heart" aria-hidden="true"></i></a>
+                                                        <a href="#" class="btn add-to-cart-btn">add to cart</a>
+                                                        <a href="#" class="btn compare-btn"><i class="fa fa-random" aria-hidden="true"></i></a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li class="product-item">
+                                        <div class="contain-product deal-layout contain-product__deal-layout">
+                                            <div class="product-thumb">
+                                                <a href="#" class="link-to-product">
+                                                    <img src="assets/images/home-03/img3.jpg" alt="dd" width="330" height="330" class="product-thumnail">
+                                                </a>
+                                                <div class="labels">
+                                                    <span class="sale-label">-50%</span>
+                                                </div>
+                                            </div>
+                                            <div class="info">
+                                                <div class="biolife-countdown" data-datetime="2020-01-03 00:00 +00:00"></div>
+                                                <b class="categories">Fresh T shirt</b>
+                                                <h4 class="product-title"><a href="#" class="pr-name">White T shirt</a></h4>
+                                                <div class="price ">
+                                                    <ins><span class="price-amount"><span class="currencySymbol">Rs</span>400.00</span></ins>
+                                                    <del><span class="price-amount"><span class="currencySymbol">Rs</span>500.00</span></del>
+                                                </div>
+                                                <div class="slide-down-box">
+                                                    <p class="message">All products are carefully selected to ensure  safety.</p>
+                                                    <div class="buttons">
+                                                        <a href="#" class="btn wishlist-btn"><i class="fa fa-heart" aria-hidden="true"></i></a>
+                                                        <a href="#" class="btn add-to-cart-btn">add to cart</a>
+                                                        <a href="#" class="btn compare-btn"><i class="fa fa-random" aria-hidden="true"></i></a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li class="product-item">
+                                        <div class="contain-product deal-layout contain-product__deal-layout">
+                                            <div class="product-thumb">
+                                                <a href="#" class="link-to-product">
+                                                    <img src="assets/images/home-03/img1.jpg" alt="dd" width="330" height="330" class="product-thumnail">
+                                                </a>
+                                                <div class="labels">
+                                                    <span class="sale-label">-50%</span>
+                                                </div>
+                                            </div>
+                                            <div class="info">
+                                                <div class="biolife-countdown" data-datetime="2020-02-05 00:00 +00:00"></div>
+                                                <b class="categories">Fresh T shirt</b>
+                                                <h4 class="product-title"><a href="#" class="pr-name">White T shirt</a></h4>
+                                                <div class="price ">
+                                                    <ins><span class="price-amount"><span class="currencySymbol">Rs</span>400.00</span></ins>
+                                                    <del><span class="price-amount"><span class="currencySymbol">Rs</span>500.00</span></del>
+                                                </div>
+                                                <div class="slide-down-box">
+                                                    <p class="message">All products are carefully selected to ensure  safety.</p>
+                                                    <div class="buttons">
+                                                        <a href="#" class="btn wishlist-btn"><i class="fa fa-heart" aria-hidden="true"></i></a>
+                                                        <a href="#" class="btn add-to-cart-btn">add to cart</a>
+                                                        <a href="#" class="btn compare-btn"><i class="fa fa-random" aria-hidden="true"></i></a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
